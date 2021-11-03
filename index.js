4e8bbe4a9e1a5cd77fa6588f37da64fb9052dfc3
derrickpennel0 <dadjei-mensah003@st.ug.edu.gh>
2021-11-03 15:14:26 +0000

    A little chnages have been made

--- index.js
+++ index.js
@@ -7,20 +7,20 @@ const port = process.env.PORT || 4501;
 const blogs = [
 
     {
-        title: "I am a your antagonist",
+        title: "I am a your hero",
         author: "Dawuda Nuraini",
         aboutAuthor: "Teacher at Adonten SHS",
         description: "There’s also a section on their that shows guys how to make a little more cash on top of their monthly day job wage, which is vital in some cases just to keep your head above water.  A lot of new dads have the added stress of not having their wives’ or girlfriends’ wage coming in each month, due to the temporary career change in being a full time mum of a baby. ",
         date: "22nd October,2021",
-        likes: 20
+        likes: 250
     },
     {
-        title: "The North Face Brand",
-        author: "Christian Mario De Ontario",
+        title: "The North Face",
+        author: "Christian Mario De Ontario De La Vega",
         aboutAuthor: "Owner of The North Face",
         description: "People across the country really like the way The North Face comes up with new products. Their socks,shirts, shorts, jerseys, shoes and tanks tops are all comfortable",
-        date: "14th July,2021",
-        likes: 13,
+        date: "1st July,2021",
+        likes: 130,
 
     },
     {
@@ -29,12 +29,12 @@ const blogs = [
         aboutAuthor: "Owner of Puma",
         description: "People across the country really like the way Puma comes up with new products. Their socks,shirts, shorts, jerseys, shoes and tanks tops are all comfortable",
         date: "10th July,2021",
-        likes: 10,
+        likes: 100,
     },
     {
         title: "The Adidas Company",
         author: "Derrick Adjei-Mensah",
-        aboutAuthor: "Owner of adidas Company",
+        aboutAuthor: "Owner of The Adidas Company",
         description: "People across the country really like the way Adidas comes up with new products. The socks,shirts, shorts, jerseys, shoes and tanks tops made by the Adidas Company are all comfortable",
         date: "16th April,2021",
         likes: 250,
